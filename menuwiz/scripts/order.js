4d5180a2412a1bcfdc301e22b37ca30e0a2baf41
Wilson Young <yyfearth@gmail.com>
2013-01-15 23:27:34 -0800

    rebuild order page remove jQuery dependency

--- menuwiz/scripts/order.js
+++ menuwiz/scripts/order.js
@@ -1,2 +1,2 @@
 // Menu Wiz - Wilson Young (yyfearth@gmail.com) - Dynamic6 @ sjsu.yyfearth.com
-(function(){var b,a;a=location.search.match(/1\d\d\d/);null==a||1!==a.length?alert("invalid order id!"):(a=a[0],b=sessionStorage["order-html-"+a],null==b?alert("invalid order content!"):($("#order_id").text(a),$("#order_content").append($(b).first().nextAll())))}).call(this);
+(function(){var a,b,c,d;b=location.search.match(/1\d\d\d/);null==b||1!==b.length?alert("invalid order id!"):(b=b[0],a=sessionStorage["order-html-"+b],null==a?alert("invalid order content!"):(d=document.getElementById("order_id"),c=document.getElementById("order_content"),d.innerText=d.textContent=b,c.innerHTML=a))})()
